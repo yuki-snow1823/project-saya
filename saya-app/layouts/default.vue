58638f777d2046f1720a1528c21cdd2e6fc1d4a2
yuki-snow1823 <magic.iroha@gmail.com>
2020-09-14 00:21:36 +0900

    endpointなどの設定

--- saya-app/layouts/default.vue
+++ saya-app/layouts/default.vue
@@ -10,26 +10,11 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      
     }
-  }
+  },
+  created() {
+      console.log("API_ENDPOINT:", process.env.API_ENDPOINT);
+  },
 }
 </script>
