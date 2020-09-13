612bc2fd0fab993f6f23aaa81d32a76f928513c3
yuki-snow1823 <magic.iroha@gmail.com>
2020-09-14 00:10:28 +0900

    アクション作成

--- saya-api/app/controllers/v1/counters_controller.rb
+++ saya-api/app/controllers/v1/counters_controller.rb
@@ -1,2 +1,15 @@
 class V1::CountersController < ApplicationController
+
+  def update
+    counter = Counter.find(1)
+    counter.count = counter.count + 1
+    counter.save
+    render json: counter
+  end
+
+  # private
+  # def counter_params
+  #   params.require(:counter).permit(:count)
+  # end
+
 end
