41b46d7a0547e5ea5364673266c785bf56dec35f
yuki-snow1823 <magic.iroha@gmail.com>
2020-09-16 01:15:58 +0900

    ボタンの追加

--- saya-api/app/controllers/v1/counters_controller.rb
+++ saya-api/app/controllers/v1/counters_controller.rb
@@ -1,8 +1,12 @@
 class V1::CountersController < ApplicationController
 
+  def show
+    @counter = Counter.find(1)
+    render json: {test: @counter}
+  end
+
   def update
     @counter = Counter.find(1)
-
     @counter.count = @counter.count + 1
     @counter.save
     render json: {test: @counter}
--- saya-app/pages/index.vue
+++ saya-app/pages/index.vue
@@ -1,16 +1,44 @@
 <template>
-  <div @click="count()">Hello Saya</div>
+  <div>
+    <v-btn @click="count()" rounded color="#FFEB3B">
+      <v-btn icon color="#FFC107">
+        <v-icon>mdi-star</v-icon>
+      </v-btn>
+      Happy Birthday Saya
+    </v-btn>
+    <div>沙耶がお祝いされた回数：{{ countSayaBirthday }}</div>
+  </div>
 </template>
 
 <script>
   import axios from "@/plugins/axios"
 
   export default {
-    methods: {
-      async count(todo) {
-        console.log("hoge")
-        await axios.put("/v1/counter")
+    data() {
+      return {
+        countSayaBirthday: 0
       }
+    },
+    methods: {
+      async count() {
+        let vm = this
+        console.log("hoge")
+        await axios.put("/v1/counter").then(response => {
+          vm = response;
+          console.log(response.data.test.count);
+          this.countSayaBirthday = response.data.test.count;
+        })
+      }
+    },
+    created() {
+      let vm = this
+      console.log("hage")
+      axios.put("/v1/counter").then(response => {
+        vm = response;
+        console.log(response.data.test.count);
+        this.countSayaBirthday = response.data.test.count;
+      })
+      this.countSayaBirthday
     },
     components: {}
   }
