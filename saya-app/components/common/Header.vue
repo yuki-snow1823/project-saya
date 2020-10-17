2c886deecc40a18f4e85897161fd3ea5421e4016
yuki-snow1823 <magic.iroha@gmail.com>
2020-10-17 14:00:44 +0900

    add:ヘッダーのコンポーネント分け

---
+++ saya-app/components/common/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col cols="12" xs="12" sm="12" offset-md="1" md="10">
+      <v-container text-center>
+        <img class="my-10" width="100%" height="100%" src="@/assets/title.png" alt="" />
+      </v-container>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+}
+</script>
