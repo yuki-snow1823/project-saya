2bb7acdaf74e36b27f5f0149e0d09c3d4f6f0231
yuki-snow1823 <magic.iroha@gmail.com>
2020-10-17 14:03:42 +0900

    add:フッターのコンポーネント分け

---
+++ saya-app/components/common/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card>
+    <v-footer class="py-3" color="light-blue lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-col class="light-blue lighten-1 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Created by Yuki Horikoshi</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
