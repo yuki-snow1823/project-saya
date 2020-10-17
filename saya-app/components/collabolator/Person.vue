4b6bed3aad3a347c06a93484c7a9e27a8194e06b
yuki-snow1823 <magic.iroha@gmail.com>
2020-10-17 14:52:59 +0900

    add:tweet関係の切り分け

---
+++ saya-app/components/collabolator/Person.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card target="_blank" href="https://twitter.com/yuki82511988" class="mb-2" color="rgba(86, 168, 165, 0.836)">
+    <v-list-item-avatar tile size="80">
+      <img class="tweet-icon" src="../../assets/yuki.png" alt="" />
+    </v-list-item-avatar>
+    <span class="twitter-name">
+      programmer
+      <span class="twitter-name">
+        Yuki
+      </span>
+    </span>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Person",
+  data () {
+    return {}
+  }
+}
+</script>
